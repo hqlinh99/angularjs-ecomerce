92315fe44089a3eaa0a59e75c4626e021e3122ac
admin <admin@hqlinh-admin.local>
2024-01-23 12:25:59 +0700

    linh-commit  23/01/2023

--- server.js
+++ server.js
@@ -12,5 +12,10 @@ app.get('/admin', function (req, res) {
     res.sendFile(path.join(__dirname, 'app/admin/index.html'));
 });
 
+const publicPath = path.join(__dirname, 'app');
+
+// Middleware để cung cấp các tệp tin tĩnh từ thư mục public
+app.use(express.static(publicPath));
+
 app.listen(8888, 'localhost');
 console.log("MyProject Server is Listening on port 8888");
