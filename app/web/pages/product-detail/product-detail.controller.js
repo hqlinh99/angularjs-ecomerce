39130347164bc416d65c753313e70e40db30c88e
luuyen01 <130760520+luuyen01@users.noreply.github.com>
2024-01-23 13:39:51 +0700

    product detail

--- app/web/pages/product-detail/product-detail.controller.js
+++ app/web/pages/product-detail/product-detail.controller.js
@@ -1,7 +1,15 @@
-myApp.controller("productDetailCtrl", ["$scope", "$timeout", "$routeParams", function ($scope, $timeout,$routeParams) {
+myApp.controller("productDetailCtrl", ["$scope", "$timeout", "$routeParams", "productFactory", function ($scope, $timeout, $routeParams, productFactory) {
     $timeout(() => {
         // <script src='layout/menu/cart.controller.js'></script>
     })
 
     console.log($routeParams.productId);
+    productFactory.getProduct($routeParams.productId).then((res) => {
+        console.log(res.data);
+        $scope.$parent.product = res.data
+    })
+
+    $scope.addToCart = (product) => {
+        $scope.$parent.notify.create({type: "success", message: "Product " + product.title + product.id + " has already been added to cart"});
+    }
 }]);
